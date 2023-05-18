b1f3c12a8b4129a78c8fa867009c630f393ab4ec
Marriett Albuquerque <marriettwt@gmail.com>
2023-05-18 17:50:16 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# APP SORTEADOR
+
+* Aplicativo destinado a realizar sorteios.
+    * Adicione uma lista de nomes no campo texto
+    * Cada nome deve ser separado por `,` uma vírgual
+    * Clique no botão e o sorteio será realizado
+    * Atenção: a aplicação não guarda os dados de cada sorteio.
+    * A cada execução do botão para sortear, a aplicação irá criar uma nova lista vazia para incluir os nomes informados no campo de texto.
---
+++ app.js
@@ -0,0 +1,32 @@
+const textArea = document.querySelector('textarea');
+const button = document.querySelector('button');
+const alertDisplay = document.getElementById('alert');
+const resultDisplay = document.getElementById('result')
+
+const nameList = [];
+
+function listNamesToArray() {
+  const textAreaValues = textArea.value.trim();
+  if (textAreaValues) {
+    let newName = textAreaValues.split(',');
+    nameList.push(... newName);
+  }
+}
+
+function draftRandomNameFromArray() {
+  if (nameList.length > 0) {
+    let namePosition = Math.floor(Math.random() * nameList.length);
+    const pickedName = nameList[namePosition];
+    alertDisplay.textContent = 'O nome sorteado foi:';
+    resultDisplay.textContent = pickedName;
+  } else {
+    alertDisplay.textContent = 'A lista de nomes está vazia. Por favor, insira nomes antes de realizar o sorteio.'
+  }
+  
+  nameList = []
+};
+
+button.addEventListener('click', () => {
+  listNamesToArray();
+  draftRandomNameFromArray();
+})
